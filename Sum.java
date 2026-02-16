d5902eb2931c47caee0c558119ea17a041a8d190
shakilbailur09-web <shakilbailur09@gmail.com>
2026-02-16 10:06:14 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,10 @@
+ classsum
+ {
+   public static void main (string args[])
+    { 
+     int a=10;
+     int b=20;
+     int sum a+b;
+     System.out.println("sum of two number is"+sum)
+       } 
+   }
